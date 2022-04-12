b7623d424a5b82b1fdadbfe8405f3db7fecc21e5
AmalieJ <amaliecille@live.dk>
2022-04-12 11:23:04 +0200

    footer - sociale medier

--- javascript/basis-footer.js
+++ javascript/basis-footer.js
@@ -1,9 +1,7 @@
-alert("hej");
-
 //Amalie JS start 
 let footerInformation = ["ADRESSE", "Vesterbro 13, 5000 Odense C", "Tlf. 61 74 39 39", "BUTIKKENS ÅBNINGSTIDER", "Mandag - fredag: 11:00 - 17:30", "Lørdag: 11:00 - 15:00", "Søndag: Lukket"];
 
 document.getElementById("address-contact").innerHTML = "<p>" + footerInformation[0] + footerInformation[1] + footerInformation[2]+ "</p>";
-
+document.getElementById("opening-hours").innerHTML = "<p>" + footerInformation[3] + footerInformation[4] + footerInformation[5] + footerInformation[6] + "</p>";
 
 //Amalie JS slut
