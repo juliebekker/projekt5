b9dee55ff5ae066f03c610aaaa6190c62d12b4f6
AmalieJ <amaliecille@live.dk>
2022-04-12 16:32:12 +0200

    footer - styling

--- javascript/basis-footer.js
+++ javascript/basis-footer.js
@@ -1,8 +1,8 @@
 //Amalie JS start 
-let footerInformation = ["ADRESSE", "Vesterbro 13, 5000 Odense C", "Tlf. 61 74 39 39", "BUTIKKENS ÅBNINGSTIDER", "Mandag - fredag: 11:00 - 17:30", "Lørdag: 11:00 - 15:00", "Søndag: Lukket"];
+let footerInformation = ["ADRESSE", "Vesterbro 13, 5000 Odense C", "TLF.", "61 74 39 39", "BUTIKKENS ÅBNINGSTIDER", "Mandag - fredag: 11:00 - 17:30", "Lørdag: 11:00 - 15:00", "Søndag: Lukket"];
 
-document.getElementById("address-contact").innerHTML = "<p>" + footerInformation[0] + "</p>" + "<p>" + footerInformation[1] + "</p>" + "<p>" + footerInformation[2] + "</p>";
-document.getElementById("opening-hours").innerHTML = "<p>" + footerInformation[3] + "</p>" + "<p>" + footerInformation[4] + "</p>" + "<p>" + footerInformation[5] + "</p>" + "<p>" + footerInformation[6] + "</p>";
+document.getElementById("address-contact").innerHTML = "<p>" + footerInformation[0] + "</p>" + "<p>" + footerInformation[1] + "</p>" + "<p>" + footerInformation[2] + "</p>" + "<p>" + footerInformation[3] + "</p>";
+document.getElementById("opening-hours").innerHTML = "<p>" + footerInformation[4] + "</p>" + "<p>" + footerInformation[5] + "</p>" + "<p>" + footerInformation[6] + "</p>" + "<p>" + footerInformation[7] + "</p>";
 
 function openSocialMedia() {
     if (document.getElementById("instagram-media").onclick) {
@@ -13,7 +13,4 @@ function openSocialMedia() {
         alert("huh?");
     }
 }
-
-
-
 //Amalie JS slut
