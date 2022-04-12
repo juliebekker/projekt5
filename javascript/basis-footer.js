fdd5eb24c8603e5a7df78c359e27b65c12dbe9ce
AmalieJ <amaliecille@live.dk>
2022-04-12 10:36:46 +0200

    Ny JS fil (basis-footer)

---
+++ javascript/basis-footer.js
@@ -0,0 +1,9 @@
+alert("hej");
+
+//Amalie JS start 
+let footerInformation = ["ADRESSE", "Vesterbro 13, 5000 Odense C", "Tlf. 61 74 39 39", "BUTIKKENS ÅBNINGSTIDER", "Mandag - fredag: 11:00 - 17:30", "Lørdag: 11:00 - 15:00", "Søndag: Lukket"];
+
+document.getElementById("address-contact").innerHTML = "<p>" + footerInformation[0] + footerInformation[1] + footerInformation[2]+ "</p>";
+
+
+//Amalie JS slut
--- javascript/basis.js
+++ javascript/basis.js
@@ -20,32 +20,3 @@ alert("tjek om js virker!");
     document.getElementById("unordered").appendChild(li);
   });
 //Cecilie JS slut
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Amalie JS start 
-let footerInformation = ["ADRESSE", "Vesterbro 13, 5000 Odense C", "Tlf. 61 74 39 39", "BUTIKKENS ÅBNINGSTIDER", "Mandag - fredag: 11:00 - 17:30", "Lørdag: 11:00 - 15:00", "Søndag: Lukket"];
-console.log(footerInformation.length);
-
-
-document.getElementById("address-contact").innerHTML = "<p>" + footerInformation[0] + footerInformation[1] + footerInformation[2]+ "</p>";
-
-
-//Amalie JS slut
