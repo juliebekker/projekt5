b4c0622c6cdb0361eab750d3344d18bef802db51
AmalieJ <amaliecille@live.dk>
2022-04-12 14:10:22 +0200

    footer - styling

--- javascript/basis-footer.js
+++ javascript/basis-footer.js
@@ -1,7 +1,7 @@
 //Amalie JS start 
 let footerInformation = ["ADRESSE", "Vesterbro 13, 5000 Odense C", "Tlf. 61 74 39 39", "BUTIKKENS ÅBNINGSTIDER", "Mandag - fredag: 11:00 - 17:30", "Lørdag: 11:00 - 15:00", "Søndag: Lukket"];
 
-document.getElementById("address-contact").innerHTML = "<p>" + footerInformation[0] + footerInformation[1] + footerInformation[2]+ "</p>";
+document.getElementById("address-contact").innerHTML = "<p>" + footerInformation[0] + footerInformation[1] + footerInformation[2] + "</p>";
 document.getElementById("opening-hours").innerHTML = "<p>" + footerInformation[3] + footerInformation[4] + footerInformation[5] + footerInformation[6] + "</p>";
 
 function openSocialMedia() {
