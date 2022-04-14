55c9f8007cdcc9a1c04e185852d9471c78936ed6
juliebekker <95928020+juliebekker@users.noreply.github.com>
2022-04-14 22:56:52 +0200

    ny

--- javascript/kontakt.js
+++ javascript/kontakt.js
@@ -25,3 +25,64 @@ function myFunction() {
     document.getElementById("result").innerHTML = "Din besked er sendt, du vil få svar over mail inden længe";
   }
 }
+
+/*Onfocus*/
+
+function myOnfocus1() {
+  var onfocus = document.getElementById('navn');
+  onfocus.value = "";
+}
+
+function myOnfocus2() {
+  var onfocus = document.getElementById('email');
+  onfocus.value = "";
+}
+
+function myOnfocus3() {
+  var onfocus = document.getElementById('emne');
+  onfocus.value = "";
+}
+
+function myOnfocus4() {
+  var onfocus = document.getElementById('besked');
+  onfocus.value = "";
+}
+
+
+/*Onblur*/
+
+function myOnblur1() {
+  var onblur = document.getElementById('navn');
+
+  if (onblur.value == "" ) {
+    onblur.value = "Navn...";
+
+  }
+}
+
+function myOnblur2() {
+  var onblur = document.getElementById('email');
+
+  if (onblur.value == "" ) {
+    onblur.value = "Email...";
+
+  }
+}
+
+function myOnblur3() {
+  var onblur = document.getElementById('emne');
+
+  if (onblur.value == "" ) {
+    onblur.value = "Emne...";
+
+  }
+}
+
+function myOnblur4() {
+  var onblur = document.getElementById('besked');
+
+  if (onblur.value == "" ) {
+    onblur.value = "Besked...";
+
+  }
+}
