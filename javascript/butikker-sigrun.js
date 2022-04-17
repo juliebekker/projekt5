5e5ce2c279cf5c0e1fdd762d935338244a41fc41
Sigrunpm <96175187+Sigrunpm@users.noreply.github.com>
2022-04-17 10:03:12 +0200

    Sigrun

--- javascript/butikker-sigrun.js
+++ javascript/butikker-sigrun.js
@@ -1,23 +1,20 @@
-
-
-
 // Alt herunder er vores galleri, til profilbilleder:
 
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
-
+ 
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
