598cc1d5dd816f82869fa38b29464d0183c85bf9
tjmaucl <tjma38750@edu.ucl.dk>
2022-04-19 08:04:25 +0200

    miljounderside js

---
+++ javascript/miljobevidsthed.js
@@ -0,0 +1,18 @@
+let samarbejdspartnere = ["- Røde kors", "- Kirkens korshær", "- Red barnet", "- Folkekirkens nødhjælp"];
+
+let citationstegn = "";
+
+samarbejdspartnere.forEach(printAfSamarbejdspartnere);
+document.getElementById("listeoverpartnere").innerHTML = citationstegn;
+
+function printAfSamarbejdspartnere(næsteelement) {
+    citationstegn += næsteelement + "<br>" + "<br>";
+}
+
+let aktivelleroverstået = "aktiv";
+
+if (aktivelleroverstået == "overstået") {
+  document.getElementById("midlertidiginformation").innerHTML = "Værd opmærksom på, at i løbet af denne måde sætter særligt fokus på hvordan man kan komme af med sit affald på den mest hensynsmæssige måde";
+} else {
+  document.getElementById("midlertidiginformation").style.display = "none";
+}
