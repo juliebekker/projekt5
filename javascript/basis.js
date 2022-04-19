d9e00e0e1f1e34f8fae87382ecf50d2ab3cf2d4f
juliebekker <95928020+juliebekker@users.noreply.github.com>
2022-04-19 11:38:28 +0200

    ny

--- javascript/basis.js
+++ javascript/basis.js
@@ -1,7 +1,7 @@
 //alert("tjek om js virker!");
 //Cecilie JS start
-  //Herunder er burgermenu lavet ved brug af "getElementById", som er hentet fra DOM. 
-  
+  //Herunder er burgermenu lavet ved brug af "getElementById", som er hentet fra DOM.
+
   let burgerMenu = document.getElementById('burgermenu');
   let overlay = document.getElementById('menu');
   burgerMenu.addEventListener('click',function(){
@@ -10,13 +10,13 @@
   });
 
 //Herunder er kommentarboks lavet ved brug af "getElementById", som også er hentet fra DOM.
-  let send= document.getElementById("post");
+/*  let send= document.getElementById("post");
   send.addEventListener("click", function(){
     let commentBoxValue= document.getElementById("comment-box").ariaValueMax;
-    
+
     let li = document.createElement("li");
     let text = document.createTextNode(commentBoxValue);
     li.appendChild(text);
     document.getElementById("unordered").appendChild(li);
-  });
+  });*/
 //Cecilie JS slut
